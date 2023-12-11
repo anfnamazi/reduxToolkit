467e96920bc750da6fcfc84a109d8e90b46329bc
anfnamazi <anf.namazi@gmail.com>
2023-12-11 22:55:12 +0330

    feat: add handle logout

--- src/common/ProtectedRoute.tsx
+++ src/common/ProtectedRoute.tsx
@@ -1,21 +1,29 @@
 import { FC } from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import { useAppSelector } from "../app/hook";
+import { useAppDispatch, useAppSelector } from "../app/hook";
+import { clear as clearToken } from "../features/token";
 
 interface ProtectedRouteProps {}
 
 const ProtectedRoute: FC<ProtectedRouteProps> = () => {
   const token = useAppSelector((state) => state.token);
+  const dispatch = useAppDispatch();
 
   if (!token) {
     console.log("redirect login");
     return <Navigate to="/login" />;
   }
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(clearToken());
+  };
+
   return (
     <div>
       protect
       <Outlet />
+      <button onClick={handleLogout}>logout</button>
     </div>
   );
 };
