57935f65c3dbdaf50cbd926ae9b825ec2c4bfa42
anfnamazi <anf.namazi@gmail.com>
2023-12-11 22:51:03 +0330

    feat: add token to local storage & set token from localstorage to redux in app rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/public/Home';
-import Primary from './pages/private/Primary';
-import Secondary from './pages/private/Secondary';
-import ProtectedRoute from './common/ProtectedRoute';
-import Login from './pages/public/Login';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/public/Home";
+import Primary from "./pages/private/Primary";
+import Secondary from "./pages/private/Secondary";
+import ProtectedRoute from "./common/ProtectedRoute";
+import Login from "./pages/public/Login";
+import { useAppDispatch } from "./app/hook";
+import { set as setToken } from "./features/token";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  const token = localStorage.getItem("token");
+  dispatch(setToken(token));
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -11,6 +11,7 @@ const Login: FC<LoginProps> = () => {
 
   const handleLogin = () => {
     dispatch(setToken("token"));
+    localStorage.setItem("token", "token");
     navigate("/primary");
   };
 
