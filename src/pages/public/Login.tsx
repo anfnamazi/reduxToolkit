2212d2846af74eda50dd9af7f94b74fc2b9809df
anfnamazi <anf.namazi@gmail.com>
2023-12-11 22:44:41 +0330

    feat: add handle login

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -1,9 +1,27 @@
 import { FC } from "react";
+import { useAppDispatch } from "../../app/hook";
+import { set as setToken } from "../../features/token";
+import { redirect, useNavigate } from "react-router-dom";
 
 interface LoginProps {}
 
 const Login: FC<LoginProps> = () => {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    dispatch(setToken("token"));
+    navigate("/primary");
+  };
+
+  return (
+    <div>
+      Login
+      <div>
+        <button onClick={handleLogin}>login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
