8eeb3fddd4b4e7649310a2a9d8bfe806cf7f0819
anfnamazi <anf.namazi@gmail.com>
2023-12-11 20:40:13 +0330

    feat: add app pages

---
+++ src/pages/Primary.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface PrimaryProps {
+    
+}
+ 
+const Primary: FC<PrimaryProps> = () => {
+    return ( <div>Primary</div> );
+}
+ 
+export default Primary;
---
+++ src/pages/Secondary.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface SecondaryProps {
+    
+}
+ 
+const Secondary: FC<SecondaryProps> = () => {
+    return ( <div>Secondary</div> );
+}
+ 
+export default Secondary;
