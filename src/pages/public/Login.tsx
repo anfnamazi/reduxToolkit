a55f4dff61bba5ccff009cd90e5ab88bf9d02ed2
anfnamazi <anf.namazi@gmail.com>
2023-12-11 22:57:35 +0330

    fix: avoid form load login page when token  is valid

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
-import { useAppDispatch } from "../../app/hook";
+import { useAppDispatch, useAppSelector } from "../../app/hook";
 import { set as setToken } from "../../features/token";
-import { redirect, useNavigate } from "react-router-dom";
+import { Navigate, redirect, useNavigate } from "react-router-dom";
 
 interface LoginProps {}
 
 const Login: FC<LoginProps> = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const token = useAppSelector(state => state.token)
+
+  if(token) {
+    return <Navigate to="/primary" />
+  }
 
   const handleLogin = () => {
     dispatch(setToken("token"));
